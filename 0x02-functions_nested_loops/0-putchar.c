50dede4fbcaf6b1439d36af669947199cd277f97
Yusuf Suleiman <yusufsuleiman14@gmail.com>
2023-02-21 03:27:55 -0800

    final 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time.h>
+/**
+ * main - A Program that prints _putchar and a new line
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
